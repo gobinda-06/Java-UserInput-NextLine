717a1c27234ff6424020a5b392b3396537020df7
Gobinda Chandra  Sahu <gobindasahu173@gmail.com>
2025-09-17 21:47:05 +0530

    Add files via upload

---
+++ Scanner_nextLine_04.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;   
+public class Scanner_nextLine_04 
+{
+    public static void main(String[] args) 
+    {
+
+        Scanner sc =new Scanner(System.in);
+        System.out.println("please enter your name");
+        String name =sc.nextLine();
+        System.out.println(name);        
+        
+    }
+    
+}
